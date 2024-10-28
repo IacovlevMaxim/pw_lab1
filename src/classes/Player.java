e7aa70451782289ce78f817f212a79c0de5fb1d4
i22moros <152692418+i22moros@users.noreply.github.com>
2024-10-28 19:02:59 +0100

    Update Player.java

--- src/classes/Player.java
+++ src/classes/Player.java
@@ -8,8 +8,9 @@ public class Player {
 	private LocalDate registration;
 	private String email;
 	
-	// METHODS
-	
+	/**
+	 * Empty constructor
+	 */
 	public Player() {
 		
 	}
@@ -28,54 +29,87 @@ public class Player {
 		this.email = email;
 	}
 	
+	/**
+	 * Gets the name of the player
+	 * @return The name of the player
+	 */
 	public String getName() {
 		return this.full_name;
 	}
-	
+	/**
+	 * Gets the birthday of the player
+	 * @return The birthday of the player
+	 */
 	public LocalDate getBirth() {
 		return this.birth;
 	}
-	
+	/**
+	 * Gets the registration date of the player
+	 * @return The registration day of the player
+	 */
 	public LocalDate getRegistration() {
 		return this.registration;
 	}
-	
+	/**
+	 * Gets the email of the player, which must be unique
+	 * @return The email of the player
+	 */
 	public String getEmail() {
 		return this.email;
 	}
-	
+	/**
+	 * Sets the name of the player
+	 * @param name New name of the player
+	 */
 	public void setName(String name) {
 		this.full_name = name;
 	}
-	
+	/**
+	 * Sets the birthday of the player
+	 * @param birth New birthday of the player
+	 */
 	public void setBirth(LocalDate birth) {
 		this.birth = birth;
 	}
-	
-	public void setReservation(LocalDate registration) {
+	/**
+	 * Sets the registration date of the player
+	 * @param registration New registration day of the player
+	 */
+	public void setRegistration(LocalDate registration) {
 		this.registration = registration;
 	}
-	
+	/**
+	 * Sets the email of the player
+	 * @param email New email of the player
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
 	
-
+	/**
+	 * Creates a string to print the information of the player
+	 * @return The string containing the information of the player
+	 */
 	public String toString() { // DO WE NEED TO SEE IF THE VARIABLES ARE EMPTY TO PRINT THEM
 		String playerInfo;
-		playerInfo = "\nPlayer's name: " + this.full_name;
+		String name = this.full_name.replaceAll("_", " ");
+		playerInfo = "\nPlayer's name: " + name;
 		playerInfo += "\nDate of birth: " + this.birth;
 		playerInfo += "\nFirst reservation: " + this.registration;
 		playerInfo += "\nEmail addres: " + this.email;
 		
 		return playerInfo;
 	}
-	
-	public int calculateSeniority() { // IT DOESN'T WORK
+	/**
+	 * Computes the seniority of the player in years
+	 * @return The years the player has been registered
+	 */
+	public int calculateSeniority() { 
 		int seniority;
 		LocalDate ahora= LocalDate.now();
 		this.registration.getYear();
 		seniority = ahora.getYear() - this.registration.getYear();
 		return seniority;
 	}
+	
 }
