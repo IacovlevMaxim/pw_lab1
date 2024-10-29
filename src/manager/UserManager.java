ba6f5ab9653001e7920ac123783f6181bd11c62c
p02gatom <91120174+p02gatom@users.noreply.github.com>
2024-10-29 12:10:51 +0100

    Update UserManager.java

--- src/manager/UserManager.java
+++ src/manager/UserManager.java
@@ -1,7 +1,7 @@
 package manager;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 
 import classes.Player;
 
@@ -16,6 +16,12 @@ public class UserManager
 		users = new ArrayList<>();
 		
 	}
+	
+	/**
+	 * Check if an user is registered in the system
+	 * @param email
+	 * @return true if the user with the email is registered, if he is not return false
+	 */
 	
 	public boolean isRegistered(String email)
 	{
@@ -35,6 +41,12 @@ public class UserManager
 		return false;
 		
 	}
+	
+	/**
+	 * Register a new user in the system
+	 * @param newUser
+	 * @return True if the user can be added to the list, false if the user is already registered
+	 */
 	
 	public boolean register(Player newUser)
 	{
@@ -56,6 +68,15 @@ public class UserManager
 		
 	}
 	
+	/**
+	 * Modify a specific user by searching him
+	 * @param user
+	 * @param newNameSurname
+	 * @param newBirthDate
+	 * @param newReservation
+	 * @param newEmail
+	 */
+	
 	public void modifyUser(Player user, String newNameSurname, Date newBirthDate, Date newReservation, String newEmail)
 	{
 		
@@ -65,9 +86,9 @@ public class UserManager
 			if(u == user)
 			{
 				
-				user.setNameSurname(newNameSurname);
+				user.setName_surname(newNameSurname);
 				user.setBirthDate(newBirthDate);
-				user.setReservation(newReservation);
+				user.setFirstReservation(newReservation);
 				user.setEmail(newEmail);
 				
 			}
@@ -75,6 +96,11 @@ public class UserManager
 		}
 		
 	}
+	
+	/**
+	 * Lists all the users in the system
+	 * @return A new list with the users in the system
+	 */
 	
 	public ArrayList<Player> listUsers()
 	{
@@ -95,7 +121,7 @@ public class UserManager
 		return regUsers;
 		
 	}
-
+	
 	//NEW
 	public Player getUser(String email) {
             for (Player user : users) {
