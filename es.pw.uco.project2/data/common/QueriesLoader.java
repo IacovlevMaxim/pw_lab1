03035d87f5fa19da186fb37224bdc84610cb81f8
i22moros <152692418+i22moros@users.noreply.github.com>
2024-11-13 16:35:55 +0100

    Create QueriesLoader.java

---
+++ es.pw.uco.project2/data/common/QueriesLoader.java
@@ -0,0 +1,32 @@
+package data.common;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class QueriesLoader {
+	private static Properties properties;
+
+	/**
+	 * Loads the files.properties file
+	 */
+	public QueriesLoader() {
+		properties = new Properties();
+		InputStream input;
+		try {
+			input =QueriesLoader.class.getClassLoader().getResourceAsStream("queries.properties");
+			properties.load(input);
+		} catch (IOException ex) {
+            ex.printStackTrace();
+        }
+	}
+	
+	/**
+	 * Gets a property
+	 * @param key The property to get
+	 * @return The equivalent value in the files.properties file
+	 */
+	public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
