2275849c12277ac8e4bb7e7aa710aa2bf55e7067
i22moros <152692418+i22moros@users.noreply.github.com>
2024-11-14 16:14:57 +0100

    Update ConfigLoader.java

--- es.pw.uco.project2/src/data/common/ConfigLoader.java
+++ es.pw.uco.project2/src/data/common/ConfigLoader.java
@@ -5,12 +5,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+/**
+ * Class in charge of loading the config.properties file
+ */
 public class ConfigLoader {
 	
 	private static Properties properties;
 
 	/**
-	 * Loads the files.properties file
+	 * Loads the config.properties file
 	 */
 	public ConfigLoader() {
 		properties = new Properties();
@@ -27,7 +30,7 @@ public class ConfigLoader {
 	/**
 	 * Gets a property
 	 * @param key The property to get
-	 * @return The equivalent value in the files.properties file
+	 * @return The equivalent value in the config.properties file
 	 */
 	public String getProperty(String key) {
         return properties.getProperty(key);
