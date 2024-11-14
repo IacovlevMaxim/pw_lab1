c12ec47cb95ec8d783d4f0b405f6318498dd45c9
i22moros <152692418+i22moros@users.noreply.github.com>
2024-11-14 19:20:47 +0100

    Update Main.java

--- es.pw.uco.project2/src/display/Main.java
+++ es.pw.uco.project2/src/display/Main.java
@@ -1,10 +1,42 @@
 package display;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in); // SCANNER TO READ THE KEYBOARD
+		String opt="1"; // OPTION FOR THE MAIN PROGRAM
+		
+		while(!opt.equals("0")) {
+			
+			System.out.println("\nWelcome to Basketball's! How would you like to log in?\n0) Exit program\n1) User Manager\n2) Court Manager\n3) Reservation Manager\n ");
+			opt = sc.next();		
+			
+			switch(opt) {
+			
+				case "1":
+					UserManagerMain.main(args);
+					break;
+				
+				case "2":
+					CourtManagerMain.main(args);
+					break;
+					
+				case "3":
+					ReservationManagerMain.main(args);
+					break;
+
+				case "0":
+					System.out.println("\nSaving changes and ending program...\n");	
+					break;
+				
+				default:
+					System.out.println("\n~ Opcion no valida ~\n");			
+					break;
+			}
+		}
+		sc.close();
 	}
-
 }
