f629cdcfe7bbb215e36f0755769254532d80ac24
i22moros <152692418+i22moros@users.noreply.github.com>
2024-11-17 14:59:28 +0100

    Update CourtNotFoundException.java

--- es.pw.uco.project2/src/business/exceptions/CourtNotFoundException.java
+++ es.pw.uco.project2/src/business/exceptions/CourtNotFoundException.java
@@ -1,15 +1,19 @@
 package business.exceptions;
 
-public class CourtNotFoundException extends Exception
-{
+public class CourtNotFoundException extends Exception{
 	
+	/**
+	 * Empty constructor
+	 */
 	public CourtNotFoundException() {}
-	
+	/**
+	 * Sends a message
+	 * @param error The message to send
+	 */
 	public CourtNotFoundException(String err)
 	{
 		
 		super(err);
 		
 	}
-
 }
