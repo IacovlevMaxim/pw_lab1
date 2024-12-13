0a086c97a34d805f2f85ec8d96bc691d86d1869e
i22moros <152692418+i22moros@users.noreply.github.com>
2024-12-14 00:11:36 +0100

    Update UserDAO.java

--- es.pw.uco.project2/src/data/DAO/UserDAO.java
+++ es.pw.uco.project2/src/data/DAO/UserDAO.java
@@ -3,11 +3,15 @@ package data.DAO;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import business.UserDTO;
 import data.common.DBConnection;
 import data.common.QueriesLoader;
 
+/**
+ * DAO for the login information of the users
+ */
 public class UserDAO {
 
 	QueriesLoader loader;
@@ -19,6 +23,11 @@ public class UserDAO {
 		loader = new QueriesLoader();
 	}
 	
+	/**
+	 * Gets an user using the email, from the databse
+	 * @param email Email of the user
+	 * @return The user information
+	 */
 	public UserDTO requestUserByEmail(String email){
 		UserDTO user = new UserDTO();
 		try {
@@ -47,5 +56,40 @@ public class UserDAO {
 			e.printStackTrace();
 		}
 		return user;
+	}
+	
+	/**
+	 * Returns all users in the database
+	 * @return The list with all the users in the database
+	 */
+	public ArrayList<UserDTO> requestAllUsers(){
+		ArrayList<UserDTO> users = new ArrayList<UserDTO>();
+		try {
+			DBConnection dbConnection = new DBConnection();
+			Connection connection = dbConnection.getConnection();
+			
+			PreparedStatement stmt = connection.prepareStatement(loader.getProperty("UsersAllFilter"));
+			
+            ResultSet rs = stmt.executeQuery();
+			
+            while(rs.next())
+            {
+            	String password = rs.getString("password");
+                String email = rs.getString("email");
+                Boolean type = rs.getBoolean("type");
+
+            	users.add(new UserDTO(email, password, type));
+            }
+            
+			if (stmt != null){ 
+				stmt.close(); 
+			}
+			dbConnection.closeConnection();
+		
+		}catch (Exception e){
+			System.err.println(e);
+			e.printStackTrace();
+		}
+		return users;
 	}
 }
