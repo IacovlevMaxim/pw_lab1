cb4c39d2ff1b3b0989250bd1e16fc954e8c1739f
Manuel Gavilán Torres <p02gatom@uco.es>
2024-12-21 16:56:26 +0100

    ViewBookingServlet

---
+++ FinalProject/src/java/ViewBookingServlet.java
@@ -0,0 +1,55 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.RequestDispatcher;
+
+import java.io.IOException;
+import java.util.list;
+
+import data.DAO.ReservationDAO;
+
+
+/**
+ * Servlet implementation class ViewBookingServlet
+ */
+@WebServlet("/ViewBookingServlet")
+public class ViewBookingServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		
+		String startDate = request.getParameter("startDate");
+		String endDate = request.getParameter("endDate");
+		
+		ReservationDAO r = new ReservationDAO();
+		
+		try
+		{
+			
+			List<Reservation> completedReservations = r.getCompletedReservations(startDate, endDate);
+			List<Reservation> futureReservations = r.getFutureReservations(startDate, endDate);
+			
+			request.setAttribute("completedReservations", completedReservations);
+			request.setAttribute("futureReservations", futureReservations);
+			
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/BookingView.jsp");
+			
+			dispatcher.forward(request, response);
+			
+		}
+		
+		catch (Exception e)
+		{
+			
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			
+		}
+		
+	}
+
+}
